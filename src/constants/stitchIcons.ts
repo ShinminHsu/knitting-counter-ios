59e10ca6fc1f6da9a3919d5b31398bd485740fbb
Shinmin Hsu <shinminhsu@gmail.com>
2026-03-15 22:22:36 +0800

    refactor: update stitch types, names, and icons per reviewed CSV
    - Replace old knit StitchType enums (WRAP_AND_TURN, CDD, CDDP, M1L/R/LP/RP,
      CABLE_1_1/2_2 variants) with new ones (BACKWARD_LOOP_CO, S2KP2, SSPP2,
      LLI/LLPI/RLI/RLPI, CABLE_2ST_RC/LC/RPC/LPC, CH3_PICOT, DTR)
    - Update StitchTypeInfo abbr fields per CSV (sc inc, hdc inc, dc inc,
      sc3inc, hdc3inc, dc3inc, 3dc-clu, 3hdc-clu, 5dc popcorn, 5dc shell,
      k1 tbl, p1 tbl, slip slip knit/purl)
    - Update CROCHET/KNITTING_STITCHES and category arrays to match CSV order
    - Update INCREASE_STITCH_TYPES: replace M1L/R variants with LLI/LLPI/RLI/RLPI
    - Update all three i18n locale files with correct names per CSV
    - Rewrite stitchIcons.ts to use new assets/knit-symbols/ folder for knitting
      SVGs and add ch3_picot + dtr to crochet SVG map
    - Update STITCH_SYMBOL map in StitchPicker.tsx to use new StitchType keys

--- src/constants/stitchIcons.ts
+++ src/constants/stitchIcons.ts
@@ -9,6 +9,8 @@ import SvgSc          from '../../assets/crochet-symbols/svg/crochet-master_sc.s
 import SvgHdc         from '../../assets/crochet-symbols/svg/crochet-master_hdc.svg'
 import SvgDc          from '../../assets/crochet-symbols/svg/crochet-master_dc.svg'
 import SvgTr          from '../../assets/crochet-symbols/svg/crochet-master_tr.svg'
+import SvgCh3Picot    from '../../assets/crochet-symbols/svg/crochet-master_ch3-picot.svg'
+import SvgDtr         from '../../assets/crochet-symbols/svg/crochet-master_dtr.svg'
 import SvgScInc       from '../../assets/crochet-symbols/svg/crochet-master_sc-inc.svg'
 import SvgHdcInc      from '../../assets/crochet-symbols/svg/crochet-master_hdcinc.svg'
 import SvgHdc3Inc     from '../../assets/crochet-symbols/svg/crochet-master_3hdc-inc.svg'
@@ -33,6 +35,8 @@ export const CROCHET_SVG_MAP: Partial<Record<StitchType, ComponentType<SvgProps>
   [StitchType.HALF_DOUBLE]:  SvgHdc,
   [StitchType.DOUBLE]:       SvgDc,
   [StitchType.TREBLE]:       SvgTr,
+  [StitchType.CH3_PICOT]:    SvgCh3Picot,
+  [StitchType.DTR]:          SvgDtr,
   [StitchType.SC_INC]:       SvgScInc,
   [StitchType.SC3INC]:       SvgSc3inc,
   [StitchType.HDC_INC]:      SvgHdcInc,
@@ -52,60 +56,58 @@ export const CROCHET_SVG_MAP: Partial<Record<StitchType, ComponentType<SvgProps>
 }
 
 // ─── Knitting (SVG) ───────────────────────────────────────────────────────────
-import SvgKnit from '../../assets/knit-chart-symbols/knit.svg'
-import SvgPurl from '../../assets/knit-chart-symbols/purl.svg'
-import SvgYarnOver from '../../assets/knit-chart-symbols/yarnover.svg'
-import SvgSlipWyib from '../../assets/knit-chart-symbols/slip.svg'
-import SvgSlipWyif from '../../assets/knit-chart-symbols/slipwyif.svg'
-import SvgSsk from '../../assets/knit-chart-symbols/decreaseleft.svg'
-import SvgSsp from '../../assets/knit-chart-symbols/decreaseleft_purl.svg'
-import SvgK2tog from '../../assets/knit-chart-symbols/decreaseright.svg'
-import SvgP2tog from '../../assets/knit-chart-symbols/decreaseright_purl.svg'
-import SvgSssk from '../../assets/knit-chart-symbols/decreaseleft.2w.svg'
-import SvgK3tog from '../../assets/knit-chart-symbols/decreaseright.2w.svg'
-import SvgCdd from '../../assets/knit-chart-symbols/decrease3to1centered.svg'
-import SvgM1l from '../../assets/knit-chart-symbols/slantleft.svg'
-import SvgM1r from '../../assets/knit-chart-symbols/slantright.svg'
-import SvgM1lp from '../../assets/knit-chart-symbols/twist_purl.svg'
-import SvgM1rp from '../../assets/knit-chart-symbols/twist_purl.svg'
-import SvgKtbl from '../../assets/knit-chart-symbols/twist.svg'
-import SvgPtbl from '../../assets/knit-chart-symbols/twist_purl.svg'
-import SvgCable11RC from '../../assets/knit-chart-symbols/crossright.svg'
-import SvgCable11LC from '../../assets/knit-chart-symbols/crossleft.svg'
-import SvgCable22RC from '../../assets/knit-chart-symbols/c2over2right.svg'
-import SvgCable22LC from '../../assets/knit-chart-symbols/c2over2left.svg'
-import SvgCable11RPC from '../../assets/knit-chart-symbols/crossright_purl.svg'
-import SvgCable11LPC from '../../assets/knit-chart-symbols/crossleft_purl.svg'
-import SvgCable22RPC from '../../assets/knit-chart-symbols/c2over2right-purl.svg'
-import SvgCable22LPC from '../../assets/knit-chart-symbols/c2over2left-purl.svg'
-import SvgBindOff from '../../assets/knit-chart-symbols/bindoff.svg'
+import SvgKnit          from '../../assets/knit-symbols/knit-master_knit.svg'
+import SvgPurl          from '../../assets/knit-symbols/knit-master_purl.svg'
+import SvgYarnOver      from '../../assets/knit-symbols/knit-master_yo.svg'
+import SvgSlipWyib      from '../../assets/knit-symbols/knit-master_sl1-wyib.svg'
+import SvgSlipWyif      from '../../assets/knit-symbols/knit-master_sl1-wyif.svg'
+import SvgBackwardLoopCo from '../../assets/knit-symbols/knit-master_backward-loop-co.svg'
+import SvgKtbl          from '../../assets/knit-symbols/knit-master_k1-tbl.svg'
+import SvgPtbl          from '../../assets/knit-symbols/knit-master_pl-tbl.svg'
+import SvgK2tog         from '../../assets/knit-symbols/knit-master_k2tog.svg'
+import SvgP2tog         from '../../assets/knit-symbols/knit-master_p2tog.svg'
+import SvgSsk           from '../../assets/knit-symbols/knit-master_ssk.svg'
+import SvgSsp           from '../../assets/knit-symbols/knit-master_ssp.svg'
+import SvgK3tog         from '../../assets/knit-symbols/knit-master_k3tog.svg'
+import SvgP3tog         from '../../assets/knit-symbols/knit-master_p3tog.svg'
+import SvgSssk          from '../../assets/knit-symbols/knit-master_sssk.svg'
+import SvgSssp          from '../../assets/knit-symbols/knit-master_sssp.svg'
+import SvgS2kp2         from '../../assets/knit-symbols/knit-master_s2kp2.svg'
+import SvgSspp2         from '../../assets/knit-symbols/knit-master_sspp2.svg'
+import SvgLli           from '../../assets/knit-symbols/knit-master_lli.svg'
+import SvgLlpi          from '../../assets/knit-symbols/knit-master_llpi.svg'
+import SvgRli           from '../../assets/knit-symbols/knit-master_rli.svg'
+import SvgRlpi          from '../../assets/knit-symbols/knit-master_rlpi.svg'
+import SvgCable2stRC    from '../../assets/knit-symbols/knit-master_2st-RC.svg'
+import SvgCable2stLC    from '../../assets/knit-symbols/knit-master_2st-LC.svg'
+import SvgCable2stRPC   from '../../assets/knit-symbols/knit-master_2st-RPC.svg'
+import SvgCable2stLPC   from '../../assets/knit-symbols/knit-master_2st-LPC.svg'
 
 export const KNIT_SVG_MAP: Partial<Record<StitchType, ComponentType<SvgProps>>> = {
-  [StitchType.KNIT]:          SvgKnit,
-  [StitchType.PURL]:          SvgPurl,
-  [StitchType.YARN_OVER]:     SvgYarnOver,
-  [StitchType.SLIP_WYIB]:     SvgSlipWyib,
-  [StitchType.SLIP_WYIF]:     SvgSlipWyif,
-  [StitchType.SSK]:           SvgSsk,
-  [StitchType.SSP]:           SvgSsp,
-  [StitchType.K2TOG]:         SvgK2tog,
-  [StitchType.P2TOG]:         SvgP2tog,
-  [StitchType.SSSK]:          SvgSssk,
-  [StitchType.K3TOG]:         SvgK3tog,
-  [StitchType.CDD]:           SvgCdd,
-  [StitchType.M1L]:           SvgM1l,
-  [StitchType.M1R]:           SvgM1r,
-  [StitchType.M1LP]:          SvgM1lp,
-  [StitchType.M1RP]:          SvgM1rp,
-  [StitchType.K_TBL]:         SvgKtbl,
-  [StitchType.P_TBL]:         SvgPtbl,
-  [StitchType.CABLE_1_1_RC]:  SvgCable11RC,
-  [StitchType.CABLE_1_1_LC]:  SvgCable11LC,
-  [StitchType.CABLE_2_2_RC]:  SvgCable22RC,
-  [StitchType.CABLE_2_2_LC]:  SvgCable22LC,
-  [StitchType.CABLE_1_1_RPC]: SvgCable11RPC,
-  [StitchType.CABLE_1_1_LPC]: SvgCable11LPC,
-  [StitchType.CABLE_2_2_RPC]: SvgCable22RPC,
-  [StitchType.CABLE_2_2_LPC]: SvgCable22LPC,
-  [StitchType.BIND_OFF]:      SvgBindOff,
+  [StitchType.KNIT]:           SvgKnit,
+  [StitchType.PURL]:           SvgPurl,
+  [StitchType.YARN_OVER]:      SvgYarnOver,
+  [StitchType.SLIP_WYIB]:      SvgSlipWyib,
+  [StitchType.SLIP_WYIF]:      SvgSlipWyif,
+  [StitchType.BACKWARD_LOOP_CO]: SvgBackwardLoopCo,
+  [StitchType.K_TBL]:          SvgKtbl,
+  [StitchType.P_TBL]:          SvgPtbl,
+  [StitchType.K2TOG]:          SvgK2tog,
+  [StitchType.P2TOG]:          SvgP2tog,
+  [StitchType.SSK]:            SvgSsk,
+  [StitchType.SSP]:            SvgSsp,
+  [StitchType.K3TOG]:          SvgK3tog,
+  [StitchType.P3TOG]:          SvgP3tog,
+  [StitchType.SSSK]:           SvgSssk,
+  [StitchType.SSSP]:           SvgSssp,
+  [StitchType.S2KP2]:          SvgS2kp2,
+  [StitchType.SSPP2]:          SvgSspp2,
+  [StitchType.LLI]:            SvgLli,
+  [StitchType.LLPI]:           SvgLlpi,
+  [StitchType.RLI]:            SvgRli,
+  [StitchType.RLPI]:           SvgRlpi,
+  [StitchType.CABLE_2ST_RC]:   SvgCable2stRC,
+  [StitchType.CABLE_2ST_LC]:   SvgCable2stLC,
+  [StitchType.CABLE_2ST_RPC]:  SvgCable2stRPC,
+  [StitchType.CABLE_2ST_LPC]:  SvgCable2stLPC,
 }
